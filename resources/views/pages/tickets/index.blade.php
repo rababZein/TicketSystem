c61ef6e989a74ddc9ded571a72024d99a8c2c48e
rababZein <rababelZen@yahoo.com>
2019-10-02 09:21:50 +0200

    frontend

---
+++ resources/views/pages/tickets/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.master')
+
+@section('title', 'Tickets Management')
+
+@section('content')
+<tickets-component></tickets-component>
+<!-- set progressbar -->
+<vue-progress-bar></vue-progress-bar>
+@endsection
