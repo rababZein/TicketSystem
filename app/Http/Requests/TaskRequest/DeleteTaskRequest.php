b3fb33237ffebf6d975b1f43316b985ab99f4c97
rababZein <rababelZen@yahoo.com>
2019-10-15 22:58:03 +0200

    delete and view

---
+++ app/Http/Requests/TaskRequest/DeleteTaskRequest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Requests\TaskRequest;
+
+use Illuminate\Foundation\Http\FormRequest;
+use App\Models\Task;
+
+class DeleteTaskRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        if (auth()->user()->isAdmin()) {
+            return true;
+        }
+
+        $task_id =$this->route('task_id');
+        $task = Task::find($task_id);
+
+        if (!$task) {
+            throw new ItemNotFoundException($task_id);
+        }
+        
+        if ($task->responsible->id == auth()->user()->id) {
+            return true;
+        }
+        
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            //
+        ];
+    }
+}
