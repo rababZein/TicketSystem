aa6d2e83821eac7c47146221aa3d19a97f1ba2a9
rababZein <rababelZen@yahoo.com>
2019-10-15 23:04:52 +0200

    view ticket

---
+++ app/Http/Requests/TicketRequest/ViewTicketRequest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Requests\TicketRequest;
+
+use Illuminate\Foundation\Http\FormRequest;
+use App\Models\Ticket;
+
+class ViewTicketRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        if (auth()->user()->isAdmin()) {
+            return true;
+        }
+
+        $ticket_id =$this->route('ticket_id');
+        $ticket = Ticket::find($ticket_id);
+
+        if (!$ticket) {
+            throw new ItemNotFoundException($ticket_id);
+        }
+
+        if ($ticket->created_by == auth()->user()->id || $ticket->project->owner->id == auth()->user()->id) {
+            return true;
+        }
+        
+        foreach ($ticket->project->assigns as $assign) {
+            if ($assign->id == auth()->user()->id) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            //
+        ];
+    }
+}
