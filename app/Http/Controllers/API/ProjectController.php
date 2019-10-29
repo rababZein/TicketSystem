aa75fcf92ea32ff9fe60b6e04338f8ca865c49f2
rababZein <rababelZen@yahoo.com>
2019-10-29 15:40:27 +0200

    add data to  project

--- app/Http/Controllers/API/ProjectController.php
+++ app/Http/Controllers/API/ProjectController.php
@@ -96,6 +96,8 @@ class ProjectController extends BaseController
       throw new ItemNotCreatedException('Project');
     }
 
+    $project->owner;
+
     return $this->sendResponse(new ProjectResource($project), 'Project created successfully.');
   }
 
