94dbe53e1693a540251618a8efb1706382bb4562
rababZein <rababelZen@yahoo.com>
2019-10-30 13:03:56 +0200

    register observer

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,8 +4,20 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
+use App\Observers\PermissionObserver;
+use App\Models\Permission;
 use App\Observers\ProjectObserver;
 use App\Models\Project;
+use App\Observers\ReceiptObserver;
+use App\Models\Receipt;
+use App\Observers\RoleObserver;
+use App\Models\Role;
+use App\Observers\TaskObserver;
+use App\Models\Task;
+use App\Observers\TicketObserver;
+use App\Models\Ticket;
+use App\Observers\UserObserver;
+use App\Models\User;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -26,7 +38,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        User::observe(UserObserver::class);
+        Role::observe(RoleObserver::class);
+        Permission::observe(PermissionObserver::class);
         Project::observe(ProjectObserver::class);
-
+        Tiket::observe(TiketObserver::class);
+        Task::observe(TaskObserver::class);
+        Receipt::observe(ReceiptObserver::class);  
     }
 }
