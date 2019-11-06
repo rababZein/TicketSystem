c67350453f4116d795ae87f6eef82c6ca5a3729b
net_66@yahoo.com <net_66@yahoo.com>
2019-11-06 16:01:00 +0200

    fix: pug - Reverting back axios to 18.1

--- resources/js/api.js
+++ resources/js/api.js
@@ -9,40 +9,6 @@ const API = axios.create({
 
 // API.defaults.headers.common['Authorization'] = token ? `Bearer ${token}` : '';
 
-// owners end point
-const owners = {
-    getAll: params => API.get('/owner/getall'),
-}
-
-// responsibles end point
-const responsibles = {
-    getAll: params => API.get('/user/getAllResponsibles'),
-}
-
-const tasks = {
-    get: params => API.get('/tasks/list/'),
-    getById: params => API.get('/tasks/' + params),
-    getAll: params => API.get('/tasks/getall'),
-    delete: params => API.delete('/tasks/' + params),
-}
-
-// tracking tasks
-const track = {
-    get: params => API.get('/tracking/' + params),
-    post: params => API.post('/tracking/' + params.task_id, {
-        comment: params.comment,
-        start_at: params.start_at,
-        task_id: params.task_id
-    }),
-    put: params => API.patch('/tracking/' + params.task_id + '/' + params.track_id, { 
-        end_at: params.end_at,
-        task_id: params.task_id
-    }),
-    delete: params => API.delete('/tracking/' + params.task_id + '/' + params.track_id),
-    countDuration: params => API.get('/tracking/' + params),
-    checkTrackingInProgress: params => API.get('/tracking/checkTrackingInProgress/' + params),
-    getHistory: params => API.get('/tracking/history/' + params),
-}
 
 // receipts end point
 const receipts = {
@@ -51,17 +17,8 @@ const receipts = {
     delete: params => API.delete('/receipts/' + params),
 }
 
-// status end point
-const status = {
-    getAll: params => API.get('/status/getAll'),
-}
+
 
 export default {
-    owners,
-    tasks,
-    responsibles,
     receipts,
-    track,
-    status,
-    tasks
 }
--- resources/js/api/index.js
+++ resources/js/api/index.js
@@ -9,32 +9,31 @@ export const API = axios.create({
 // will check if the global handler should be used or not (by default is true)
 // Example: API.get('/v-api/api-endpoint', { handlerEnabled: false })
 const isHandlerEnabled = (config = {}) => {
-    return config.hasOwnProperty("handlerEnabled") && !config.handlerEnabled
-        ? false
-        : true;
+    return config.hasOwnProperty("handlerEnabled") && !config.handlerEnabled ? false : true;
 };
 
+
 const errorHandler = error => {
-    switch (isHandlerEnabled(error.config)) {
-        case error.response.status == 401:
-            window.location.href = "/login";
-            break;
-        case error.response.status == 403:
+    if (isHandlerEnabled(error.config)) {
+        // check if unauthorized error returned
+        if (error.response.status === 401) {
+            window.location.href = '/login';
+        }
+        if (error.response.status == 403) {
             Toast.fire({
                 type: "error",
                 title: "This action is unauthorized"
             });
-            break;
-        case error.response.status == 404:
-            router.push("/404");
-            break;
-        case error.response.status == 500:
+        }
+        if (error.response.status === 404) {
+            router.push('/404')
+        }
+        if (error.response.status == 500) {
             Toast.fire({
                 type: "error",
                 title: "Server error!"
             });
-            break;
-        default:
+        }
     }
     return Promise.reject({ ...error });
 };
