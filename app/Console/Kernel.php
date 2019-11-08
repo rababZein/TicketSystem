46a9601fe84b20f4bb1b05c006006d7d72571120
net_66@yahoo.com <net_66@yahoo.com>
2019-11-08 10:39:52 +0200

    fix command name "refreshMailBoxToCreateTickets"

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('minute:updateMailBox')
+        $schedule->command('minute:refreshMailBoxToCreateTickets')
                  ->everyFiveMinutes();
     }
 
