e85eb9175a42da46364c5dcdd61007195f52042f
rababZein <rababelZen@yahoo.com>
2019-11-17 11:54:31 +0200

    add event updating

--- app/Observers/TaskObserver.php
+++ app/Observers/TaskObserver.php
@@ -61,6 +61,16 @@ class TaskObserver
     }
 
     /**
+     * Handle the task "updating" event.
+     *
+     * @param  \App\Task  $task
+     * @return void
+     */
+    public function updating(Task $task)
+    {
+    }
+
+    /**
      * Handle the task "deleted" event.
      *
      * @param  \App\Task  $task
