55b6275303257dd5e48e6565c2f76735c5d6f702
rababZein <rababelZen@yahoo.com>
2019-11-20 11:07:57 +0200

    attachment setting

--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -9,7 +9,7 @@ class Ticket extends Model
 
     protected $table = 'tickets';
     public $timestamps = false;
-    protected $fillable = array('created_at', 'updated_at','name', 'description', 'project_id', 'status_id', 'email_id', 'created_by', 'updated_by');
+    protected $fillable = array('created_at', 'updated_at', 'name', 'description', 'project_id', 'status_id', 'email_id', 'created_by', 'updated_by');
 
     public function project()
     {
@@ -34,6 +34,11 @@ class Ticket extends Model
     public function ticket_status()
     {
         return $this->hasOne('App\Models\Status', 'id', 'status_id');
+    }
+
+    public function files()
+    {
+        return $this->hasMany('App\Models\Ticket_file');
     }
 
     public function ownTickets($id)
