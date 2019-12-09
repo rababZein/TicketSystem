6165d5c0b7c86fe3b0d08b7222bf5318dd011f59
net_66@yahoo.com <net_66@yahoo.com>
2019-12-09 11:13:28 +0200

    fix logo link

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -1,5 +1,5 @@
 <!-- Brand Logo -->
-<a href="/" class="brand-link">
+<a href="/admin" class="brand-link">
     <img src="{{ asset('assets/img/ALFerp.png') }}" alt="ALFerp Logo" class="brand-image img-circle elevation-3"
         style="opacity: .8">
     <span class="brand-text font-weight-light">{{ config('app.name', 'ALFerp') }}</span>
