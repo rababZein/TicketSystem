2b6f1819b7b332d487f06194eebcc04e6a5118cf
rababZein <rababelZen@yahoo.com>
2019-12-12 11:14:40 +0200

    remove un-needed

--- app/Http/Controllers/API/TaskController.php
+++ app/Http/Controllers/API/TaskController.php
@@ -217,23 +217,15 @@ class TaskController extends BaseController
     foreach ($tasks->toArray()['data'] as $task) {
       $project['name'] = $task['project']['name'];
          
-      $statusFlag = false;
       $i=0;
       if (isset($project['columns'])) {
         foreach($project['columns'] as $status) {
           if ($status['name'] == $task['task_status']['name']) {
             $project['columns'][$i]['tasks'][] = $task;
-            $statusFlag = true;
           }
           $i++;
         }
       }
-      // if (! $statusFlag) {
-      //   $status = [];
-      //   $status['name'] = $task['task_status']['name'];
-      //   $status['tasks'][] = $task;
-      //   $project['columns'][] = $status;
-      // } 
     }
     
     return $this->sendResponse($project, 'Tasks retrieved successfully.');
