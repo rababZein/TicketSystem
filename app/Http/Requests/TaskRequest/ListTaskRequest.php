9fe6591bd8c3c23ca7910253d109737308d68ff3
rababZein <rababelZen@yahoo.com>
2019-12-17 14:11:29 +0200

    validation

--- app/Http/Requests/TaskRequest/ListTaskRequest.php
+++ app/Http/Requests/TaskRequest/ListTaskRequest.php
@@ -16,6 +16,22 @@ class ListTaskRequest extends FormRequest
         return true;
     }
 
+
+    /**
+     * Inject GET parameters into validation data
+     *
+     * @param array $keys Properties to only return
+     *
+     * @return array
+     */
+    public function all($keys = null)
+    {
+        $data = parent::all($keys);
+        $data = json_decode($this->get('queryParams'), true);
+
+        return $data;
+    }
+
     /**
      * Get the validation rules that apply to the request.
      *
@@ -24,7 +40,19 @@ class ListTaskRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            "sort" => "required|array",
+            "sort.*.name" => "string",
+            "sort.*.order" => "string",
+            "filters"  => "required|array",
+            "filters.*.type" => "string",
+            "filters.*.mode" => "string",
+            "filters.*.selected_options" => "string",
+            "filters.*.name" => "string",
+            "filters.*.text" => "string",
+            "global_search" => "required|string",
+            "per_page" => "required|integer",
+            "page" => "required|integer",
+
         ];
     }
 }
