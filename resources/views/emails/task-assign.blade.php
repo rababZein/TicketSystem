5fe3c779078ce9d0b10bb518ddb7958828b19efe
net_66@yahoo.com <net_66@yahoo.com>
2020-01-16 17:37:48 +0200

    add new email layout and fix html description in task mail

---
+++ resources/views/emails/task-assign.blade.php
@@ -0,0 +1,38 @@
+@extends('emails.layout')
+
+@section('content')
+<strong>
+@if ($notifiable->metadata)
+    @if ($notifiable->metadata->gender == 'male')
+        {{ __('Mail/Intro.male', ['name' => $notifiable->name]) }}
+    @elseif($notifiable->metadata->gender == 'female')
+        {{ __('Mail/Intro.female', ['name' => $notifiable->female]) }}
+    @else
+        {{ __('Mail/Intro.unknown', ['name' => $notifiable->name]) }}
+    @endif
+@else
+    {{ __('Mail/Intro.unknown', ['name' => $notifiable->name]) }}
+@endif
+</strong>
+<p>
+    {!! __('Mail/Task/TaskAssignNotification.taskName', ['task_name' => $task->name]) !!}
+</p>
+<p>
+    {!! __('Mail/Task/TaskAssignNotification.description', ['description' => $task->description]) !!}
+</p>
+<table role="presentation" border="0" cellpadding="0" cellspacing="0" class="btn btn-primary">
+    <tbody>
+      <tr>
+        <td align="center">
+          <table role="presentation" border="0" cellpadding="0" cellspacing="0">
+            <tbody>
+              <tr>
+                <td> <a href="{{ url('/admin/task/'.$task->id) }}" target="_blank">{{__('Mail/Task/TaskAssignNotification.seeMore')}}</a> </td>
+              </tr>
+            </tbody>
+          </table>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+@endsection
